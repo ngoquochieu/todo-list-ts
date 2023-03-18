0c9955d9d34f9cd29ef5d325b491c0a75e992c84
ngoquochieu <ngoquochieu06112001@gmail.com>
2023-03-18 15:29:48 +0700

    Set up

---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div className="col-6">
+
+        </div>
+    )
+}
---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,10 @@
+import { Avatar } from "antd"
+export default function UserInfo() {
+    return(
+        <div className="col-6">
+            <div className="head-info">
+                <Avatar/>
+            </div>
+        </div>
+    )
+}
